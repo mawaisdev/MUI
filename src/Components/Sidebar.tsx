7e9a4e893608b063985803665c95503ed97ce050
Muhammad Awais <m.awais2030@gmail.com>
2023-09-13 15:21:27 +0500

    SideBar Design Commplete

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,14 +1,96 @@
-import { Box } from '@mui/material'
-
+import {
+  Article as ArticleIcon,
+  Home as HomeIcon,
+  People as PeopleIcon,
+  Person as PersonIcon,
+  Settings as SettingsIcon,
+  Storefront as StorefrontIcon,
+  AccountCircleRounded as AccountCircleRoundedIcon,
+  ModeNightOutlined as ModeNightOutlinedIcon,
+} from '@mui/icons-material'
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material'
 export const Sidebar = () => {
   return (
     <Box
-      bgcolor={'skyblue'}
       flex={1}
+      flexDirection={'column'}
       p={2}
       sx={{ display: { xs: 'none', sm: 'block' } }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/home'>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary='Home' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/pages'>
+            <ListItemIcon>
+              <ArticleIcon />
+            </ListItemIcon>
+            <ListItemText primary='Pages' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/groups'>
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary='Groups' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/store'>
+            <ListItemIcon>
+              <StorefrontIcon />
+            </ListItemIcon>
+            <ListItemText primary='MarketPlace' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/friends'>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary='Friends' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/home'>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary='Settings' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={'a'} href='/home'>
+            <ListItemIcon>
+              <AccountCircleRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary='Profile' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ModeNightOutlinedIcon />
+            </ListItemIcon>
+            <Switch defaultChecked />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   )
 }
